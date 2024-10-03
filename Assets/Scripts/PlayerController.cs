1a2ae408ec4bd6e35374173bd0fe7f4724910d09
citus-eye <anykabolin@gmail.com>
2024-10-03 11:37:08 +1000

    Added Pickups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   
+
     public float speed = 1.0f;
     private Rigidbody rb;
+    private int pickupCount;
 
     void Start()
     {
         //Gets the Rigibody component attached to this game object 
         rb = GetComponent<Rigidbody>();
+        //gets the number of pickups in our scene
+        pickupCount = GameObject.FindGameObjectsWithTag("Pickup").Length;
+        // run the check pick ups function
+        CheckPickups();
     }
 
     void FixedUpdate()
@@ -27,6 +32,33 @@ public class PlayerController : MonoBehaviour
         //add force to our rigibody from our movement vector times speed veriable
         rb.AddForce(movement * speed * Time.deltaTime);
 
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Pickup")
+        {
+            //destroy the collided object
+            Destroy(other.gameObject);
+            //decoment the pick up count
+            pickupCount--;
+            // run the check pickup function again
+            CheckPickups();
+        }
+    }
+
+    private void CheckPickups()
+    {
+        print("pickups left: " + pickupCount);
+        if(pickupCount == 0)
+        {
+            WinGame();
+        }
+    }
+
+    private void WinGame()
+    {
+        print("You are Victorious");
     }
 }
